890c6b6c58e0085a8efe812c61cf1fd7744fd3a4
NICOLASDAGOSTINI <NICOLASDAGOSTINI@10.10.111.35>
2022-10-20 09:13:03 +0200

    esercizio21 client

--- esercizio21client/src/esercizio21client/Esercizio21client.java
+++ esercizio21client/src/esercizio21client/Esercizio21client.java
@@ -4,6 +4,12 @@
  */
 package esercizio21client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  * @author NICOLASDAGOSTINI
@@ -14,8 +20,57 @@ public class Esercizio21client {
     /**
      * @param args the command line arguments
      */
+    
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    int vm=0;
+
+    public Esercizio21client() throws IOException {
+        socket = new Socket("127.0.0.1", 13);
+        socket.setSoTimeout(1000000);
+        out = new DataOutputStream(socket.getOutputStream());
+        in = new DataInputStream(socket.getInputStream());
+    }
+
+    public void inviaScelta(int scelta) throws IOException {
+        out.writeInt(scelta);
+    }
+    
+    public void inviaRicevi(int scelta) throws IOException {
+        out.writeInt(scelta);
+        
+    }
+    public void riceviVoto() throws IOException {
+        
+        for(int i=0;i<5;i++){
+            int voto = in.readInt();
+            System.out.println("voto del "+ (i+1) +" caffe: " + voto);
+            vm+=voto;
+        }
+        System.out.println("voto media: " + vm/5);
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int scelta = 0;
+        int voto;
+        Scanner input = new Scanner(System.in);
+        try {
+            Esercizio21client client = new Esercizio21client();
+            while (true) {
+                for(int i=0;i<5;i++){
+                    System.out.println("vota del "+ (i+1) +" caffe: ");
+                    voto = input.nextInt();
+                    client.inviaScelta(voto);
+                }
+                client.inviaRicevi(scelta);
+                client.riceviVoto();
+                
+                System.exit(0);
+                
+            }
+        } catch (IOException ex) {
+        }
     }
     
 }
